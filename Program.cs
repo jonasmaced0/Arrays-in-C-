e604b6906abcbb731bedd101c54b61aba1cce3bf
jonas macedo <jonasvbo@gmail.com>
2023-10-30 22:54:47 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Array_ArrayList_List\n");
+
+
+int[] numeros;
+numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+string[] nomes;
+
+nomes = new string[5] { "Maria", "Eva", "Davi", "Pedro", "Marcelo" };
+
+
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+Console.WriteLine(nomes[2]);
+Console.WriteLine(nomes[3]);
+Console.WriteLine(nomes[4]);
+
+// declarando um array
+
+int[] numeros1;
+
+// alocar memória
+
+numeros1 = new int[3];
+
+numeros1[0] = 1;
+numeros1[1] = 2;
+numeros1[2] = 3;
+
+Console.ReadKey();
